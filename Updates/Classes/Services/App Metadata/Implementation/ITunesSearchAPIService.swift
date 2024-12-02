2a17d7157530517c1f396c6fb0bd3afc628410cf
Stefan Edler <office@stefanedler.com>
2024-12-02 20:45:11 +0100

    Update ITunesSearchAPIService.swift

--- Updates/Classes/Services/App Metadata/Implementation/ITunesSearchAPIService.swift
+++ Updates/Classes/Services/App Metadata/Implementation/ITunesSearchAPIService.swift
@@ -15,13 +15,6 @@ struct ITunesSearchAPIService: AppMetadataService {
     /// Parses iTunes Search API responses.
     private let parsingService = ITunesSearchJSONParsingService()
 
-    private lazy var urlSession: URLSession = {
-        let urlSession = URLSession(configuration: .ephemeral)
-        urlSession.configuration.requestCachePolicy = .reloadIgnoringLocalAndRemoteCacheData
-        urlSession.configuration.urlCache = nil
-        return urlSession
-    }()
-
     init?(bundleIdentifier: String, countryCode: String) {
         let lowercasedCountryCode = countryCode.lowercased()
         let urlString = "http://itunes.apple.com/lookup?bundleId=\(bundleIdentifier)&country=\(lowercasedCountryCode)"
@@ -43,6 +36,9 @@ struct ITunesSearchAPIService: AppMetadataService {
 
     func fetchAppMetadata(_ completion: @escaping (AppMetadataResult) -> Void) {
         DispatchQueue.global(qos: .background).async {
+            let urlSession = URLSession(configuration: .ephemeral)
+            urlSession.configuration.requestCachePolicy = .reloadIgnoringLocalAndRemoteCacheData
+            urlSession.configuration.urlCache = nil
             let urlRequest = URLRequest(url: self.iTunesSearchAPIURL, cachePolicy: .reloadIgnoringLocalAndRemoteCacheData)
             let task = urlSession.dataTask(with: urlRequest) { data, _, error in
                 if let data  {
